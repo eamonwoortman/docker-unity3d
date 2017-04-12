6c0c3d6b5a92c5c3bc332a85d6b638b3f932366e
Eamon Woortman <eamon.woortman@gmail.com>
2017-04-12 09:59:43 +0200

    Changed Unity version to 5.4.0p1

--- get-unity.sh
+++ get-unity.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-
 URL=http://download.unity3d.com/download_unity/linux/
-PKG=unity-editor-5.3.4f1+20160316_amd64.deb
+PKG=unity-editor-5.4.0p1+20160810_amd64.deb
 
 echo "Downloading Unity3D installer..."
 curl -o /app/unity_editor.deb -s "${URL}${PKG}" 
