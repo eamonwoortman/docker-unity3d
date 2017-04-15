c680d39bd4d527c0c0bc97396a7e6e7303535089
Eamon Woortman <eamon.woortman@gmail.com>
2017-04-15 12:17:45 +0200

    Updated unity package to 5.5.0f3

--- get-unity.sh
+++ get-unity.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 URL=http://download.unity3d.com/download_unity/linux/
-PKG=unity-editor-5.4.0p1+20160810_amd64.deb
+PKG=unity-editor-5.5.0f3+20161125_amd64.deb
 
 echo "Downloading Unity3D installer..."
 curl -o /app/unity_editor.deb -s "${URL}${PKG}" 
